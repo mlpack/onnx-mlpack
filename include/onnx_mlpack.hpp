3e0a805e15ddd71e4b511e7baef8fd7d8f798aa5
Ryan Curtin <ryan@ratml.org>
2026-06-26 12:55:08 -0400

    Fix include ordering.

--- include/onnx_mlpack.hpp
+++ include/onnx_mlpack.hpp
@@ -11,13 +11,13 @@
 #include <stdint.h>
 #include <mlpack.hpp>
 
-#include "onnx_mlpack/convert.hpp"
-
 // If the mlpack version is too old, we need to include the backported Scale
 // layer.
 #if MLPACK_VERSION_MAJOR <= 4 || \
     (MLPACK_VERSION_MAJOR == 4 && MLPACK_VERSION_MINOR <= 8)
   #include "onnx_mlpack/mlpack_backport/scale.hpp"
 #endif
+
+#include "onnx_mlpack/convert.hpp"
 
 #endif
